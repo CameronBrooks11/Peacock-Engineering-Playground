12a88f080c0a84959b5de6914dcc2949b292190f
Cameron Brooks <cambrooks3393@gmail.com>
2024-02-29 20:19:09 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Leap71.ShapeKernel;
+using PicoGK;
+
+string strOutputFolder = "C:/Users/Cameron/Peacock-Engineering-Playground/Out";
+
+try
+{
+    PicoGK.Library.Go(
+        0.1f,
+        Leap71.ShapeKernelExamples.BaseCylinderShowCase.Task,
+        strOutputFolder
+        );
+}
+catch (Exception e)
+{
+    Console.WriteLine("Failed to run Task.");
+    Console.WriteLine(e.ToString());
+}
